9b7d3b47e99f21e73b9cc18881640406eccb6bd7
isadora <161738122+iF08163742@users.noreply.github.com>
2024-08-01 12:26:01 -0300

    gjbihjn

--- main.js
+++ main.js
@@ -6,31 +6,17 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-        enunciado: "Assim que você sai do cemitério logo após o enterro a noite, você se depara com uma noiva de vestido branco com alguns rasgos, o que você faz?
+        enunciado: "Assim que você sai do cemitério logo após o enterro a noite, você se depara com uma noiva de vestido branco com alguns rasgos, o que você faz?"
         alternativas: [
         "Eu fico totalmente paralisado", 
         "Eu volto correndo para trás"
         ]
     },
     {
-        enunciado: "Pergunta 1,
+        enunciado: "Depois que você conseguiu sair de dentro do cemitério, você vai sair do estacionamento do cemitério e seu carro não liga, o que você faz?"
        alternativas: [
-             "Alternativa 1, 
-             Alternativa 2"
-       ]
-   },
-   {
-       enunciado: "Pergunta 1,
-       alternativas: [
-         "Alternativa 1, 
-         Alternativa 2"
-      ]
-   },
-   {
-        enunciado: "Pergunta 1,
-         alternativas: [
-             "Alternativa 1, 
-             Alternativa 2"
+             "Entro em mais um desespero e acabo tendo uma crise de pânico"
+             "Eu recorro ao meu celular e ligo para alguém vir me socorrer"
        ]
    },
       ];

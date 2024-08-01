08264cd14ecef90ea29aa02699f08821aee58d92
isadora <161738122+iF08163742@users.noreply.github.com>
2024-08-01 12:18:12 -0300

    jhjghjghjgh

---
+++ main.js
@@ -0,0 +1,36 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+{
+        enunciado: "Assim que você sai do cemitério logo após o enterro a noite, você se depara com uma noiva de vestido branco com alguns rasgos, o que você faz?
+        alternativas: [
+        "Eu fico totalmente paralisado", 
+        "Eu volto correndo para trás"
+        ]
+    },
+    {
+        enunciado: "Pergunta 1,
+       alternativas: [
+             "Alternativa 1, 
+             Alternativa 2"
+       ]
+   },
+   {
+       enunciado: "Pergunta 1,
+       alternativas: [
+         "Alternativa 1, 
+         Alternativa 2"
+      ]
+   },
+   {
+        enunciado: "Pergunta 1,
+         alternativas: [
+             "Alternativa 1, 
+             Alternativa 2"
+       ]
+   },
+      ];

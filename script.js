78cae2303245865e542db9c80a1053b2f4eebdfe
isadora <161738122+iF08163742@users.noreply.github.com>
2024-09-12 12:23:01 -0300

    jkyhyryhyjtyjytjyt

--- script.js
+++ script.js
@@ -7,27 +7,39 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Morreu um familiar seu e você foi ao sepultamento, ao final do enterro você se encaminha em direção ao portão de saída e se depara com uma noiva com vestido branco todo sujo de sangue. O que você faz??",
-        alternativas: [
+        _alternativas: [
             {
                 texto: "Eu fecho os olhos e passo correndo.",
                 afirmação: "afirmação"
-            }
+            },
             {
-                texto: "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente."
+                texto: "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente.",
                 afirmação: "afirmação"
             }
-        ]
+        ],
+        get alternativas() {
+            return this._alternativas;
+        },
+        set alternativas(value) {
+            this._alternativas = value;
+        },
     }
     {
         enunciado: "Ufa, você conseguiu entrar em seu carro, porém ao dar partida você nota que ele não está ligando e aquela noiva que estava na saída do cemitério está vindo em direção ao su carro. O que você faz??",
         alternativas: [
             {
                 texto: "Eu entro em crise e saio correndo sem direção e abandono o carro."
-                afirmação: "afirmação"
+                _afirmação: "afirmação",
+                get afirmação() {
+                    return this._afirmação;
+                },
+                set afirmação(value) {
+                    this._afirmação = value;
+                },
             }
             {
                 texto: "Eu tento o mais rápido possível ligar para alguém que possa me socorrer."
-                afirmação: "afirmação"
+                _afirmação: "afirmação"
             }
         ]
     }
@@ -49,7 +61,7 @@ for(const alternativa of perguntaAtual.alternativas){
     botaoAlternativas.addEventListener("click", function(){
         atual++;
         mostraPergunta();
-    })
+    });
 caixaAlternativas.appendChild(botaoAlternativas);
 }
 }

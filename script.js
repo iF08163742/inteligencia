008ba3176bbedea8786b08a7838f71d7cd368f18
isadora <161738122+iF08163742@users.noreply.github.com>
2024-10-24 11:42:24 -0300

    njdffbvuierhviod

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Morreu um familiar seu e você foi ao sepultamento, ao final do enterro você se encaminha em direção ao portão de saída e se depara com uma noiva com vestido branco todo sujo de sangue. O que você faz??"[
-            alternativas: [
+            alternativas, [
             {
                 texto: "Eu fecho os olhos e passo correndo.",
                 afirmacao: "bdgfsdfsd"
@@ -15,15 +15,15 @@ const perguntas = [
             },
             {
                 [
-                texto: "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente.",
-                afirmacao: "afirmação"
+                texto, "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente.",
+                afirmacao, "afirmação"
                 ]
             }
         ],
         }
     {
         enunciado: "Ufa, você conseguiu entrar em seu carro, porém ao dar partida você nota que ele não está ligando e aquela noiva que estava na saída do cemitério está vindo em direção ao su carro. O que você faz??",
-        alternativas: [
+        alternativas, [
             {
                 texto: "Eu entro em crise e saio correndo sem direção e abandono o carro."
                 afirmacao: "afirmação",
@@ -39,6 +39,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual]
@@ -50,13 +51,15 @@ function mostraAlternativas(){
 for(const alternativa of perguntaAtual.alternativas){
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa.texto;
-    botaoAlternativas.addEventListener("click", function(){
-        atual++;
-        mostraPergunta();
-    });
+    botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
 caixaAlternativas.appendChild(botaoAlternativas);
 }
 }
 
-
+function respostaSelecionada(opçaoSelecionada){
+    const afirmacoes = opçaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();

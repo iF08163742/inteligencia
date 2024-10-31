f6a1ace420701ca725d7c735d1b7405528fe65d3
isadora <161738122+iF08163742@users.noreply.github.com>
2024-10-31 12:18:37 -0300

    ghcrdxfvbjk

--- script.js
+++ script.js
@@ -4,39 +4,38 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
+const perguntas =
     {
         enunciado: "Morreu um familiar seu e você foi ao sepultamento, ao final do enterro você se encaminha em direção ao portão de saída e se depara com uma noiva com vestido branco todo sujo de sangue. O que você faz??"[
             alternativas, [
             {
                 texto: "Eu fecho os olhos e passo correndo.",
                 afirmacao: "afirmação"
-                ]
-            },
-            {
+            }
+        ],
+            
                 [
                 texto, "Eu caminho um pouco mais rápido e passo sem olhar para os lados junto fazendo uma reza na mente.",
                 afirmacao, "afirmação"
                 ]
-            }
+            
         ],
-        }
-    {
-        enunciado: "Ufa, você conseguiu entrar em seu carro, porém ao dar partida você nota que ele não está ligando e aquela noiva que estava na saída do cemitério está vindo em direção ao su carro. O que você faz??",
-        alternativas, [
-            {
-                texto: "Eu entro em crise e saio correndo sem direção e abandono o carro."
-                afirmacao: "afirmação",
-               
-            }
-            {
-                texto: "Eu tento o mais rápido possível ligar para alguém que possa me socorrer."
-                afirmacao: "afirmação"
-            }
-        ]
     }
-];
-
+    {
+        enunciado, "Ufa, você conseguiu entrar em seu carro, porém ao dar partida você nota que ele não está ligando e aquela noiva que estava na saída do cemitério está vindo em direção ao su carro. O que você faz??"[
+        alternativas, [
+                {
+                texto: "Eu entro em crise e saio correndo sem direção e abandono o carro.",
+                afirmacao: "afirmação",
+                }
+            ],
+    
+                [
+                texto: "Eu tento o mais rápido possível ligar para alguém que possa me socorrer.",
+                afirmacao: "afirmação",
+                ]
+            ],
+            }   
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
@@ -69,7 +68,9 @@ function respostaSelecionada(opçaoSelecionada){
 }
 
 function mostraResultado(){
-    
+caixaPerguntas.textContent = "em 2050...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
